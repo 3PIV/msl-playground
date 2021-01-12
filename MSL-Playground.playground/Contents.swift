858f476672c5a28db06f5d92adb42b2854829b29
Preston Provins <pprovins@vols.utk.edu>
2021-01-12 08:43:42 -0500

    Initial Upload

---
+++ MSL-Playground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import MetalKit
+import PlaygroundSupport
+
+let frame = NSRect(x: 0, y: 0, width: 640, height: 576)
+let delegate = MetalView()
+let view = MTKView(frame: frame, device: delegate.device)
+view.delegate = delegate
+PlaygroundPage.current.liveView = view
